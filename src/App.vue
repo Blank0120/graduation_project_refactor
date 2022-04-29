7e4042f0ca46fb5a25b2942ae264de8ac5a8c7c4
tourist <17322619133@163.com>
2022-04-29 22:53:41 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <header_display @rec_to_app="rec_to_books"></header_display>
+      <search @search_list = "showSearch"></search>
+      <books :rec="rec_json" :search = "search_list"></books>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { header_display } from "./components/header.vue";
+import { search } from "./components/search.vue";
+import { book } from "./components/book.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    header_display,
+    search,
+    book
+  },
+	data(){
+		return {
+			rec_json:{},
+			search_list:{}
+		}
+	},
+	methods:{
+		rec_to_books(rec_json){
+			// console.log(rec_json);
+			console.log("rec_to_books");
+			this.rec_json = rec_json
+		},
+		showSearch(search_list){
+			console.log("search_list");
+			this.search_list = search_list
+		}
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0%;
+  margin: 0%;
 }
+
 </style>
